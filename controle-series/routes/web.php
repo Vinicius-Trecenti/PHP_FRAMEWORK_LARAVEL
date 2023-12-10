25443f22ae1f736d52b08f22b3175ae0faf41884
Vinicius <vinitrecente5@gmail.com>
2023-12-10 18:18:31 -0300

    roteando para pagina inicial

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [SeriesController::class, 'index']);
 
 Route::get('/ola', function () {
     echo 'Ola Mundo, laravel';
