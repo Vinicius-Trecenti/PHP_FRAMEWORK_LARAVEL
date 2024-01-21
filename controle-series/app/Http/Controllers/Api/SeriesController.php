7cf0204a088a9168399644473c8d0e98ffcdef9c
Vinicius <vinitrecente5@gmail.com>
2024-01-21 13:39:09 -0300

    Entendo o problema do with

--- controle-series/app/Http/Controllers/Api/SeriesController.php
+++ controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -25,11 +25,15 @@ class SeriesController extends Controller
         return response()->json($this->seriesRepository->add($request), 201);
     }
 
-    public function show(Series $series){
+    public function show(int $series){
         //$series = Series::whereId($series)->with("seasons.episodes")->first(); ou get
-        return $series;
+        $seriesModel = Series::with('seasons.episodes')->find($series);
+        if($seriesModel === null){
+            return response()->json(['message' => 'Series not found'], 404);
+        }
+        return $seriesModel;
     }
-
+ 
     public function destroy(int $series){
         Series::destroy($series);
         return response()->noContent();
