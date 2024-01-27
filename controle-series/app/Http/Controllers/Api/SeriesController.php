f35871cbca8cd0c90b9d050257df10fed867173e
Vinicius <vinitrecente5@gmail.com>
2024-01-26 23:06:29 -0300

    formas de consulta

--- controle-series/app/Http/Controllers/Api/SeriesController.php
+++ controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -15,8 +15,15 @@ class SeriesController extends Controller
 
     }
 
-    public function index(){
-        return Series::all();
+    public function index(Request $request){
+        $query = Series::query();
+
+        if($request->has('nome')){
+            return $query->where('nome', $request->nome);
+        }
+
+
+        return $query->paginate(5);
     }
 
     public function store(SeriesFormRequest $request){
